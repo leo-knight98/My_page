0ba3247e53dce687b0fd88ac8ec97c7ce4dd2bb2
Leo_98 <82750536+leo-knight98@users.noreply.github.com>
2023-01-10 17:59:10 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+class Project {
+    img = ["img/Ohia.png", "img/Dragons_den.png"]
+    name = ["Ohia", "Dragon's Den"]
+    date = ["December 2021", "January 2023"]
+    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!"]
+    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den"]
+
+    showProjects() {
+        let prj = '';
+
+        for(let i = 0; i < this.img.length; i++) {
+            prj += '<div class="project"><img class="prj_img" src="' + this.img[i] + '"><div class="prj_text"><h3>'+ this.name[i] +'</h3><p>' + this.date[i] + '<p> ' + this.tagline[i] + '<br><button><a href="' + this.link[i] + '" target="_blank" rel="noopener noreferrer"">View project</a></button></div></div>';
+        }
+
+        return prj;
+    }
+}
+
+
+window.onload = function() {
+    let project = new Project()
+    let projectContainer = document.getElementById('projectContainer');
+    projectContainer.innerHTML = project.showProjects();
+}
