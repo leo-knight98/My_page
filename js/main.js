3f560c18e391e0ee078772e0eff5d777cdd38d0b
Leo_98 <82750536+leo-knight98@users.noreply.github.com>
2023-01-18 11:57:00 +0100

    To do tracker

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Project {
-    img = ["img/Ohia.png", "img/Dragons_den.png"]
-    name = ["Ohia", "Dragon's Den"]
-    date = ["December 2021", "January 2023"]
-    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!"]
-    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den"]
+    img = ["img/Ohia.png", "img/Dragons_den.png", "img/Tracker.png"]
+    name = ["Ohia", "Dragon's Den", "To do manager"]
+    date = ["December 2021", "January 2023", "January 2023"]
+    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!", "Manage your tasks, with their due dates and color coding to show their importance."]
+    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den", "https://github.com/leo-knight98/To-do-list"]
 
     showProjects() {
         let prj = '';
