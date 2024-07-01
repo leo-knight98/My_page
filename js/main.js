aee663fcb6713b586ff9d07307b75a26bde8f2bb
Leo_98 <82750536+leo-knight98@users.noreply.github.com>
2024-07-01 22:33:45 +0200

    La Magalla

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Project {
-    img = ["img/Ohia.png", "img/Dragons_den.png", "img/Tracker.png"]
-    name = ["Ohia", "Dragon's Den", "To do manager"]
-    date = ["December 2021", "January 2023", "January 2023"]
-    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!", "Manage your tasks, with their due dates and color coding to show their importance."]
-    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den", "https://github.com/leo-knight98/To-do-list"]
+    img = ["img/Ohia.png", "img/Dragons_den.png", "img/Tracker.png", "img/La_Magalla.png"]
+    name = ["Ohia", "Dragon's Den", "To do manager", "La Magalla"]
+    date = ["December 2021", "January 2023", "January 2023", "February 2024"]
+    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!", "Manage your tasks, with their due dates and color coding to show their importance.", "Landing page for a local producer of fruit and vegetables."]
+    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den", "https://github.com/leo-knight98/To-do-list", "https://lamagalla.cat"]
 
     showProjects() {
         let prj = '';
