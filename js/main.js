6e896705244dbcabde24cfc6271d63915192d45c
Leo_98 <82750536+leo-knight98@users.noreply.github.com>
2025-01-02 12:59:40 +0100

    Add my finance

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Project {
-    img = ["img/Ohia.png", "img/Dragons_den.png", "img/Tracker.png", "img/La_Magalla.png"]
-    name = ["Ohia", "Dragon's Den", "To do manager", "La Magalla"]
-    date = ["December 2021", "January 2023", "January 2023", "February 2024"]
-    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!", "Manage your tasks, setting up their due dates, and use color coding to show their importance.", "Landing page for a local producer of fruit and vegetables, with all the necessary information."]
-    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den", "https://github.com/leo-knight98/To-do-list", "https://lamagalla.cat"]
+    img = ["img/Ohia.png", "img/Dragons_den.png", "img/Tracker.png", "img/La_Magalla.png", "img/finance.png"]
+    name = ["Ohia", "Dragon's Den", "To do manager", "La Magalla", "My finance"]
+    date = ["December 2021", "January 2023", "January 2023", "February 2024", "December 2024"]
+    tagline = ["Get to the flag, using each character only one time. They all have different abilities and you must combine them.", "Invest your way back into your world by funding people's projects and reaching 1.000.000 gold coins!", "Manage your tasks, setting up their due dates, and use color coding to show their importance.", "Landing page for a local producer of fruit and vegetables, with all the necessary information.", "Web application to manage your own finances, spending habits and saving goals. Keep that information all in one place"]
+    link = ["https://ryuggha.itch.io/ohia", "https://leo-knight98.itch.io/dragons-den", "https://github.com/leo-knight98/To-do-list", "https://lamagalla.cat", "https://my-finance-red.vercel.app"]
 
     showProjects() {
         let prj = '';
